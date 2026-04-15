2f40a92acacfa998aa2c5ebdc514fd3ee28159f8
Nguyen Trung Hieu <23013003@st.phenikaa-uni.edu.vn>
2026-04-15 17:40:43 +0700

    Add JUnit tests for path coverage

--- src/test/java/NumberProcessorTest.java
+++ src/test/java/NumberProcessorTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import src.main.java.NumberProcessor;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberProcessorTest {
@@ -8,4 +11,25 @@ public class NumberProcessorTest {
         int result = NumberProcessor.process(3);
         assertEquals(-2, result);
     }
+    
+    @Test
+    public void testZero() {
+        assertEquals(0, NumberProcessor.process(0));
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(-1, NumberProcessor.process(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(1, NumberProcessor.process(2));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(-2, NumberProcessor.process(3));
+    }
+}
 }
